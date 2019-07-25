dd0af0de2a8ac706e7d0ca32a1e0e62f0981006e
Chris Zosa <zosachris@gmail.com>
2019-07-25 12:26:08 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// Herokus opportunity to pass us runtime configuration. 
+// Configured after deployment
+const PORT = process.env.PORT || 5000;
+
+// '/' instructs express to allow user to visit root route.
+// callback is called after request is sent to '/'.
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
