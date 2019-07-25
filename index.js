ae6d33cc1b4525b0f2bfebd125b7202ff6517a9a
Chris Zosa <zosachris@gmail.com>
2019-07-25 16:33:26 -0700

    Google oath setup

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-// Herokus opportunity to pass us runtime configuration. 
-// Configured after deployment
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token:', accessToken);
+      console.log('refresh token:', refreshToken);
+      console.log('profile:', profile);
+    }
+  )
+);
+
+// 1. this route lets user sign in
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+// 2. user has signed in* this route already has the "code=4" sent to server
+// 3. Then generates access token. This will save user to database
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
-
-// '/' instructs express to allow user to visit root route.
-// callback is called after request is sent to '/'.
-app.get('/', (req, res) => {
-  res.send({ new: 'content' });
-});
-
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
