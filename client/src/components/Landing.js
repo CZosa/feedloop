20f86fb5f47f663ce9562e1237eaf25d38738263
Chris Zosa <zosachris@gmail.com>
2019-08-16 18:32:17 -0700

    adding LandingContent component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import LandingContent from './LandingContent';
+import Background from '../images/background-img.jpg';
+
+const loadingContainer = {
+  backgroundImage: `url(${Background})`,
+  backgroundSize: 'cover',
+  backgroundPosition: '50%',
+  position: 'absolute',
+  left: '0',
+  width: '100%',
+  height: '100%'
+}
+
+const bottomLeft = {
+  position: 'absolute',
+  bottom: '0',
+  marginLeft: '10px',
+  marginBottom: '10px'
+}
 
 const Landing = () => {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>
-        ESurvey
-      </h1>
-      Collect feedback from users
+  return (         
+    <div style={loadingContainer}>
+      <LandingContent />    
+      <div style={bottomLeft}>
+        FeedLoop ©2019
+      </div>
     </div>
   );
 };
