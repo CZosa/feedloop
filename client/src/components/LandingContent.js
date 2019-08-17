fa8fc0c555629f76469c772e20f7818e32aef42a
Chris Zosa <zosachris@gmail.com>
2019-08-16 18:34:25 -0700

    component holds page info

---
+++ client/src/components/LandingContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LandingContent = () => {
+  return (
+    <div className="container white-text">
+      <h3 style={{ textAlign: 'center' }}>
+        <i style={{ fontSize: '100px' }} className="material-icons">link</i>
+      </h3>
+      <div style={{ fontSize: '20px', fontStyle: 'italic' }}>
+        Your #1 Source for Collecting Feedback
+      </div>
+    </div>
+  );
+};
+
+export default LandingContent;
